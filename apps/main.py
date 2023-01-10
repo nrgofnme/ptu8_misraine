1e9a491b0b9762c923a5addb32d4dfd7daca0364
devignasrocys <devignasrocys@gmail.com>
2023-01-10 15:47:42 +0200

    Naujas failas ir programos kodas

--- apps/main.py
+++ apps/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from apps.destytojas import Destytojas
+from apps.igno_app import IgnoApp
 
 class MainApp():
     def __init__(self, main):
@@ -7,11 +8,16 @@ class MainApp():
         self.f_catalog = Frame(self.main)
         self.l_pasirinkimai = Label(self.f_catalog, text="Pasirinkite")
         self.b_destytojas = Button(self.f_catalog, text="Destytojas", command=self.run_destytojas)
-        
+        self.b_my_app = Button(self.f_catalog, text="Igno programa",command=self.run_igno_app)
         self.l_pasirinkimai.pack(side=TOP)
         self.b_destytojas.pack()
+        self.b_my_app.pack()
         self.f_catalog.pack()
 
     def run_destytojas(self):
         self.window_destytojas = Toplevel(self.main)
         self.app = Destytojas(self.window_destytojas)
+    
+    def run_igno_app(self):
+        self.window_my_app = Toplevel(self.main)
+        self.app = IgnoApp(self.window_my_app)
