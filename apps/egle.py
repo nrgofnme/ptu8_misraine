70ced8be6f3e2c0c8422044a6b40c36996a4283a
eglemot <egle.mot97@gmail.com>
2023-01-10 23:44:40 +0700

    egle.py has been created, added to main.py

---
+++ apps/egle.py
@@ -0,0 +1,48 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+
+class Egle():
+
+    def __init__(self, main):
+        self.main = main
+        self.main.title("Eglės skaičiuoklė")
+        self.langas = Frame(self.main)
+        self.langas.geometry = ("1200x300")
+        self.langas.configure(bg="#F3EFE0")
+        photo = ImageTk.PhotoImage(Image.open('images/egle/kmi.png'))
+        px=10
+        py=10
+
+        def kmi_skaiciuokle():
+            ugis = int(self.ugis_ivestis.get())
+            svoris = int(self.svoris_ivestis.get())
+            kmi = svoris / (ugis/100)**2
+            self.parodymas['text'] = f'{kmi:.1f}'
+       
+        
+
+        self.teiginys = Label(self.langas, text="PASISKAIČIUOKITE SAVO KŪNO MASĖS INDEKSĄ", relief=SUNKEN, bg="#22A39F", fg="#F3EFE0", font=('Helvetica bold', 24))
+        self.u_ugis = Label(self.langas, text="ĮVESKITE SAVO ŪGĮ CM.: ", border=5, bg="#22A39F", fg="#F3EFE0", font=('Helvetica bold', 14))
+        self.ugis_ivestis = Entry(self.langas, bd=4, relief=SUNKEN, bg="#22A39F", width=5)
+        self.s_svoris = Label(self.langas, text="ĮVESKITE SAVO SVORĮ KG.: ", border=5, bg="#22A39F", fg="#F3EFE0", font=('Helvetica bold', 14))
+        self.svoris_ivestis = Entry(self.langas, bd=4, relief=SUNKEN, bg="#22A39F", width=5)
+        self.skaiciuoti = Button(self.langas, text="SKAIČIUOTI", command=kmi_skaiciuokle, bd=10, highlightbackground="#F3EFE0", fg="#22A39F", font=('Helvetica bold', 20))
+        self.parodymas = Label(self.langas, text="", relief=SUNKEN, bd=10, bg="#22A39F")
+        self.paveiksliukas = Label(self.main, image=photo)
+        self.langas.bind('<Return>', kmi_skaiciuokle)
+
+        
+        self.teiginys.grid(row=0, column=0, padx=px, pady=py)
+        self.u_ugis.grid(row=3, column=0, padx=px, pady=py)
+        self.ugis_ivestis.grid(row=4, column=0, padx=1, pady=1)
+        self.s_svoris.grid(row=1, column=0, padx=px, pady=py)
+        self.svoris_ivestis.grid(row=2, column=0, padx=1, pady=1)
+        self.skaiciuoti.grid(row=5, column=0, padx=px, pady=py)
+        self.parodymas.grid(row=6, column=0, padx=px, pady=py)
+        self.langas.pack()
+        self.paveiksliukas.pack()
+
+        self.langas.mainloop()
+
--- apps/main.py
+++ apps/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from apps.destytojas import Destytojas
+from apps.egle import Egle
 
 class MainApp():
     def __init__(self, main):
@@ -7,11 +8,17 @@ class MainApp():
         self.f_catalog = Frame(self.main)
         self.l_pasirinkimai = Label(self.f_catalog, text="Pasirinkite")
         self.b_destytojas = Button(self.f_catalog, text="Destytojas", command=self.run_destytojas)
+        self.b_egle = Button(self.f_catalog, text="Eglė", command=self.run_egle)
         
         self.l_pasirinkimai.pack(side=TOP)
         self.b_destytojas.pack()
+        self.b_egle.pack()
         self.f_catalog.pack()
 
     def run_destytojas(self):
         self.window_destytojas = Toplevel(self.main)
         self.app = Destytojas(self.window_destytojas)
+    
+    def run_egle(self):
+        self.window_egle = Toplevel(self.main)
+        self.app = Egle(self.window_egle)
