1e847aa4362a992fd671560e37c228dbccb1b178
Kestutis Januskevicius <infohata@gmail.com>
2023-01-11 13:52:58 +0200

    added icon to exe file

--- misraine.py
+++ misraine.py
@@ -1,8 +1,12 @@
 from tkinter import *
 from apps.main import MainApp
+from PIL import ImageTk, Image
 
 def run_main_app():
     root = Tk()
+    icon = ImageTk.PhotoImage(Image.open('salad.ico'))
+    root.call('wm', 'iconphoto', root._w, icon)
+    # root.iconbitmap(bitmap=r'salad.ico')
     root.title("PTU8 Mišrainė")
     root.geometry("200x500")
     app = MainApp(root)
