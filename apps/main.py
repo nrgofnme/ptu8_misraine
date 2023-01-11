a2ac4785fdfca25b97090c299c1602cbe80e2814
airidalabas <airidajuraitiene@gmail.com>
2023-01-11 09:12:55 +0200

    main lango redagavimas2

--- apps/main.py
+++ apps/main.py
@@ -8,10 +8,12 @@ class MainApp():
         self.f_catalog = Frame(self.main)
         self.l_pasirinkimai = Label(self.f_catalog, text="Pasirinkite")
         self.b_destytojas = Button(self.f_catalog, text="Destytojas", command=self.run_destytojas)
+        self.airida_mygtukas = Button(self.main, text="Airida", command=self.run_airida)
         
         self.l_pasirinkimai.pack(side=TOP)
         self.b_destytojas.pack()
         self.f_catalog.pack()
+        self.airida_mygtukas.pack()
 
     def run_destytojas(self):
         self.window_destytojas = Toplevel(self.main)
