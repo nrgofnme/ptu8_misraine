c4cf25654f23034924083c5b764a4fd83bbea6d8
Redasmarc <marcinkus420@gmail.com>
2023-01-11 10:03:14 +0200

    add redas profile with a pic

--- apps/main.py
+++ apps/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from apps.destytojas import Destytojas
+from apps.redas import Redas
 
 class MainApp():
     def __init__(self, main):
@@ -7,10 +8,16 @@ class MainApp():
         self.f_catalog = Frame(self.main)
         self.l_pasirinkimai = Label(self.f_catalog, text="Pasirinkite")
         self.b_destytojas = Button(self.f_catalog, text="Destytojas", command=self.run_destytojas)
+        self.b_redas = Button(self.f_catalog, text="Redas", command=self.run_redas)
         
         self.l_pasirinkimai.pack(side=TOP)
         self.b_destytojas.pack()
         self.f_catalog.pack()
+        self.b_redas.pack()
+
+    def run_redas(self):
+        self.window_redas = Toplevel(self.main)
+        self.app = Redas(self.window_redas)
 
     def run_destytojas(self):
         self.window_destytojas = Toplevel(self.main)
